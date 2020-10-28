a1ce6451ba818b51201338cab08f6f612f2aa09a
Burak Saraloglu <buraksaraloglu1@gmail.com>
2020-10-28 04:42:39 +0300

    minimax integration

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,5 +1,89 @@
-const xClass = 'x';
-const circleClass = 'circle';
+let initGame = () => {
+	cellElements.forEach((cell) => {
+		board.push(cell);
+		cell.classList.remove(human);
+		cell.classList.remove(ai);
+		cell.removeEventListener('click', handleClick);
+		cell.addEventListener('click', handleClick, { once: true });
+	});
+	turn = ai;
+	bestMove();
+	setBoardHoverClass();
+	resultPage.classList.remove('show');
+};
+
+let handleClick = (e) => {
+	const cell = e.target;
+	placeMark(cell);
+	swapTurns();
+};
+
+// Swapping Turns
+let swapTurns = () => {
+	turn === ai ? (turn = human) : bestMove();
+	setBoardHoverClass();
+};
+
+// Showing moves on mouse hover
+let setBoardHoverClass = () => {
+	if (turn === human) {
+		gameBox.classList.remove(ai);
+		gameBox.classList.add(human);
+	} else {
+		gameBox.classList.remove(human);
+		gameBox.classList.add(ai);
+	}
+};
+
+// Placing Mark
+let placeMark = (cell) => {
+	cell.classList.add(turn);
+	finalControl();
+};
+
+// Check if there is winning combination on board
+let checkWinner = (turn) => {
+	return winCombinations.some((combination) => {
+		return combination.every((index) => {
+			return cellElements[index].classList.contains(turn);
+		});
+	});
+};
+
+let isDraw = () => {
+	return [ ...cellElements ].every((cell) => {
+		return cell.classList.contains(human) || cell.classList.contains(ai);
+	});
+};
+
+let endGame = (tie) => {
+	tie ? (winningText.innerText = 'Draw!') : (winningText.innerText = `${turn === human ? 'X' : 'O'} wins.`);
+	tie
+		? (resultPage.style.backgroundColor = 'rgba(64, 64, 64, 0.9)')
+		: turn === human
+			? (resultPage.style.backgroundColor = 'rgba(70, 142, 41, 0.9)')
+			: (resultPage.style.backgroundColor = 'rgba(155, 13, 13, 0.9)');
+	resultPage.classList.add('show');
+};
+
+let finalControl = () => {
+	if (checkWinner(turn)) {
+		endGame(false);
+	} else if (isDraw()) {
+		endGame(true);
+	}
+};
+
+//Default Selectors
+const cellElements = document.querySelectorAll('[data-cell]');
+const gameBox = document.getElementById('gameBox');
+const resetBtn = document.getElementById('restartButton');
+const winningText = document.querySelector('[data-result-message-text]');
+const resultPage = document.getElementById('resultMessage');
+let turn;
+let board = [];
+const human = 'x';
+const ai = 'circle';
 const winCombinations = [
 	[ 0, 1, 2 ],
 	[ 3, 4, 5 ],
@@ -10,111 +94,8 @@ const winCombinations = [
 	[ 0, 4, 8 ],
 	[ 2, 4, 6 ]
 ];
-let currentBoard = [];
 
-let circleTurn;
-
-let initGame = () => {
-	circleTurn = false;
-	cellElements.forEach((cell) => {
-		cell.classList.remove(xClass);
-		cell.classList.remove(circleClass);
-		cell.removeEventListener('click', handleClick);
-		cell.addEventListener('click', handleClick, { once: true });
-	});
-	setBoardHoverClass();
-	resultPage.classList.remove('show');
-};
-
-let handleClick = (e) => {
-	const cell = e.target;
-	const currentClass = circleTurn ? circleClass : xClass;
-	placeMark(cell, currentClass);
-	controller(currentClass);
-};
-
-let controller = (currentClass) => {
-	if (checkWin(currentClass)) {
-		endGame(false);
-	} else if (isDraw()) {
-		endGame(true);
-	} else {
-		swapTurns();
-		setBoardHoverClass();
-	}
-};
-
-// Place Mark
-let placeMark = (cell, currentClass) => {
-	cell.classList.add(currentClass);
-};
-
-// Swapping Turns
-let swapTurns = () => {
-	circleTurn = !circleTurn;
-	if (circleTurn) computerMove(circleClass);
-};
-
-// Showing moves on mouse hover
-let setBoardHoverClass = () => {
-	gameBox.classList.remove(xClass);
-	gameBox.classList.remove(circleClass);
-	circleTurn ? gameBox.classList.add(circleClass) : gameBox.classList.add(xClass);
-};
-
-let checkWin = (currentClass) => {
-	return winCombinations.some((combination) => {
-		return combination.every((index) => {
-			return cellElements[index].classList.contains(currentClass);
-		});
-	});
-};
-
-let isDraw = () => {
-	return [ ...cellElements ].every((cell) => {
-		return cell.classList.contains(xClass) || cell.classList.contains(circleClass);
-	});
-};
-
-let endGame = (draw) => {
-	draw ? (winningText.innerText = 'Draw!') : (winningText.innerText = `${circleTurn ? 'O' : 'X'} wins.`);
-	resultPage.classList.add('show');
-	circleTurn
-		? (resultPage.style.backgroundColor = 'rgba(155, 13, 13, 0.9)')
-		: (resultPage.style.backgroundColor = 'rgba(70, 142, 41, 0.9)');
-};
-
-// Random generated computer move
-// TODO: Integrate Minimax Algorithm
-let computerMove = (currentClass) => {
-	let availableCells = [];
-
-	// j = available index numbers.
-	let j = 0;
-	// Getting Available Cells
-	for (let i of cellElements) {
-		if (!i.classList.contains(xClass) && !i.classList.contains(circleClass)) {
-			availableCells.push({ j, i });
-		}
-		j++;
-	}
-	let randIndex = Math.floor(Math.random() * (availableCells.length - 1));
-	console.log(availableCells);
-	availableCells[randIndex].i.classList.add(circleClass);
-	controller(currentClass);
-};
-
-//Default Selectors
-const cellElements = document.querySelectorAll('[data-cell]');
-const gameBox = document.getElementById('gameBox');
-const resetBtn = document.getElementById('restartButton');
-const winningText = document.querySelector('[data-result-message-text]');
-const resultPage = document.getElementById('resultMessage');
-
-cellElements.forEach((cell) => {
-	cell.addEventListener('click', handleClick, { once: true });
-});
-
+initGame();
 resetBtn.addEventListener('click', initGame);
 
 //Styling the game box
---
+++ assets/js/minimax.js
@@ -0,0 +1,64 @@
+let bestMove = () => {
+	turn = ai;
+	let bestScore = -Infinity;
+	let move;
+	let j;
+	for (let i = 0; i < cellElements.length; i++) {
+		if (!cellElements[i].classList.contains(human) && !cellElements[i].classList.contains(ai)) {
+			cellElements[i].classList.add(ai);
+			tempTurn = human;
+			let score = minimax(cellElements, 0, false);
+			cellElements[i].classList.remove(ai);
+			if (score > bestScore) {
+				bestScore = score;
+				move = i;
+			}
+		}
+		j++;
+	}
+	placeMark(cellElements[move]);
+
+	swapTurns();
+};
+
+let scores = {
+	X: -100,
+	O: 100,
+	tie: 0
+};
+let tempTurn;
+
+let minimax = (board, depth, isMaximizing) => {
+	let result = checkWinner(tempTurn);
+	if (result) {
+		if (turn === human) {
+			return scores.X;
+		} else {
+			return scores.O;
+		}
+	}
+
+	if (isMaximizing) {
+		let bestScore = -Infinity;
+		for (let i = 0; i < board.length; i++) {
+			if (!board[i].classList.contains(human) && !board[i].classList.contains(ai)) {
+				board[i].classList.add(ai);
+				let score = minimax(board, depth + 1, false);
+				board[i].classList.remove(ai);
+				bestScore = Math.max(score, bestScore);
+			}
+		}
+		return bestScore;
+	} else {
+		let bestScore = Infinity;
+		for (let i = 0; i < board.length; i++) {
+			if (!board[i].classList.contains(human) && !board[i].classList.contains(ai)) {
+				board[i].classList.add(human);
+				let score = minimax(board, depth + 1, true);
+				board[i].classList.remove(human);
+				bestScore = Math.min(score, bestScore);
+			}
+		}
+		return bestScore;
+	}
+};
